Inspirati/doctis

commit 8d23fb80151cfef9068befe4f65e766a7d0973ec

Show PHP exception in REST only if detailed errors is ON

api/rest/index.php
@@ -45,7 +45,9 @@ ApiObjectFactory::$soap = false;
 
 # Show SLIM detailed errors according to Mantis settings
 $t_config = array();
-if( ON == config_get_global( 'show_detailed_errors' ) ) {
+
+$t_show_detailed_errors = ON == config_get_global( 'show_detailed_errors' );
+if( $t_show_detailed_errors ) {
 	$t_config['settings'] = array( 'displayErrorDetails' => true );
 }
 
@@ -74,7 +76,11 @@ $t_container['errorHandler'] = function( $p_container ) {
 		$t_error_to_log =  $p_exception->getMessage() . "\n" . $t_stack_as_string;
 		error_log( $t_error_to_log );
 
-		return $p_response->withStatus( HTTP_STATUS_INTERNAL_SERVER_ERROR )->withJson( $t_data );
+		if( $t_show_detailed_errors ) {
+			$p_response = $p_response->withJson( $t_data );
+		}
+
+		return $p_response->withStatus( HTTP_STATUS_INTERNAL_SERVER_ERROR );
 	};
 };
 

commit 0c3dc4190e6ac42f5cda6204e9a112740f7cf2e3

Fix 'Undefined variable' PHP notice in API REST
Since the Slim Container's 'displayErrorDetails' setting is set to true
according to $t_show_detailed_errors's value, instead of referencing the
variable directly within the closure via a 'use' statement, we retrieve
the Container's settings to determine whether PHP exception details
should be shown or not.

Remove $t_show_detailed_errors variable as it is no longer needed.

Regression introduced by 8d23fb80151cfef9068befe4f65e766a7d0973ec.

Fixes #25429

api/rest/index.php
@@ -46,8 +46,7 @@ ApiObjectFactory::$soap = false;
 # Show SLIM detailed errors according to Mantis settings
 $t_config = array();
 
-$t_show_detailed_errors = ON == config_get_global( 'show_detailed_errors' );
-if( $t_show_detailed_errors ) {
+if( ON == config_get_global( 'show_detailed_errors' ) ) {
 	$t_config['settings'] = array( 'displayErrorDetails' => true );
 }
 
@@ -76,7 +75,8 @@ $t_container['errorHandler'] = function( $p_container ) {
 		$t_error_to_log =  $p_exception->getMessage() . "\n" . $t_stack_as_string;
 		error_log( $t_error_to_log );
 
-		if( $t_show_detailed_errors ) {
+		$t_settings = $p_container->get('settings');
+		if( $t_settings['displayErrorDetails'] ) {
 			$p_response = $p_response->withJson( $t_data );
 		}
 