Inspirati/doctis

commit 8b28db636a31e04f3db1eb4d3aae5d3489a46301

Function to display formatted project selection links
New project_link_for_menu() function added to Project API, to avoid code
duplication when displaying project selection links in the navbar menu
and the project menu bar.

Handles the indentation logic based on the subproject's level in the
hierarchy, selection of the active project as well as context-specific
formatting ($p_class parameter).

Fixes #26887, #26888

core/layout_api.php
@@ -601,16 +601,16 @@ function layout_navbar_projects_list( $p_project_id = null, $p_include_all_proje
 	echo '<ul class="list dropdown-yellow no-margin">';
 
 	if( $p_include_all_projects && $p_filter_project_id !== ALL_PROJECTS ) {
-		echo ALL_PROJECTS == $p_project_id ? '<li class="active">' : '<li>';
-		echo '<a href="' . helper_mantis_url( 'set_project.php' ) . '?project_id=' . ALL_PROJECTS . '">';
-		echo lang_get( 'all_projects' ) . ' </a></li>' . "\n";
+		echo '<li>';
+		echo project_link_for_menu( ALL_PROJECTS, false, 'project_link' );
+		echo "</li>\n";
 		echo '<li class="divider"></li>' . "\n";
 	}
 
 	foreach( $t_project_ids as $t_id ) {
 		echo 0 == strcmp( $t_id, $p_project_id ) ? '<li class="active">' : '<li>';
-		echo '<a href="' . helper_mantis_url( 'set_project.php' ) . '?project_id=' . $t_id . '"';
-		echo ' class="project-link"> ' . string_attribute( project_get_field( $t_id, 'name' ) ) . ' </a></li>' . "\n";
+		echo project_link_for_menu( $t_id, false, 'project_link' );
+		echo "</li>\n";
 		layout_navbar_subproject_option_list( $t_id, $p_project_id, $p_filter_project_id, $p_trace );
 	}
 
@@ -633,6 +633,7 @@ function layout_navbar_subproject_option_list( $p_parent_id, $p_project_id = nul
 	array_push( $p_parents, $p_parent_id );
 	$t_user_id = auth_get_current_user_id();
 	$t_project_ids = user_get_accessible_subprojects( $t_user_id, $p_parent_id );
+	$t_indent = str_repeat( '&nbsp;', 4 );
 
 	foreach( $t_project_ids as $t_id ) {
 		if( $p_trace ) {
@@ -642,9 +643,8 @@ function layout_navbar_subproject_option_list( $p_parent_id, $p_project_id = nul
 		}
 
 		echo 0 == strcmp( $p_project_id, $t_full_id ) ? '<li class="active">' : '<li>';
-		echo '<a href="' . helper_mantis_url( 'set_project.php' ) . '?project_id=' . $t_full_id . '"';
-		echo ' class="project-link"> ' . str_repeat( '&#160;', count( $p_parents ) * 4 );
-		echo string_attribute( project_get_field( $t_id, 'name' ) ) . '</a></li>' . "\n";
+		echo project_link_for_menu( $t_id, false, 'project_link', $p_parents, $t_indent );
+		echo "</li>\n";
 
 		layout_navbar_subproject_option_list( $t_id, $p_project_id, $p_filter_project_id, $p_trace, $p_parents );
 	}

core/html_api.php
@@ -520,24 +520,31 @@ function html_end() {
 
 /**
  * Print the menu bar with a list of projects to which the user has access
+ *
+ * @see $g_show_project_menu_bar
+ *
  * @return void
  */
 function print_project_menu_bar() {
 	$t_project_ids = current_user_get_accessible_projects();
 	$t_current_project_id = helper_get_current_project();
+	$t_button_classes = 'btn btn-xs btn-white btn-info';
 
 	echo '<div class="col-md-12 col-xs-12">' . "\n";
 	echo '<div class="btn-group">' . "\n";
 
-	$t_active = ALL_PROJECTS == $t_current_project_id ? 'active' : '';
-	echo '<a class="btn btn-xs btn-white btn-info ' . $t_active .
-		'" href="' . helper_mantis_url( 'set_project.php?project_id=' . ALL_PROJECTS ) . '">', lang_get( 'all_projects' ), '</a>' . "\n";
-
+	echo project_link_for_menu(
+			ALL_PROJECTS,
+			$t_current_project_id == ALL_PROJECTS,
+			$t_button_classes
+		);
 	foreach( $t_project_ids as $t_id ) {
-		$t_active = $t_id == $t_current_project_id ? 'active' : '';
-		echo '<a class="btn btn-xs btn-white btn-info ' . $t_active .
-			'" href="' . helper_mantis_url( 'set_project.php?project_id=' . $t_id ) . '">', string_html_specialchars( project_get_field( $t_id, 'name' ) ), '</a>' . "\n";
-		print_subproject_menu_bar( $t_current_project_id, $t_id, $t_id . ';' );
+		echo project_link_for_menu(
+				$t_id,
+				$t_current_project_id == $t_id,
+				$t_button_classes
+			);
+		print_subproject_menu_bar( $t_current_project_id, $t_id, array( $t_id ) );
 	}
 
 	echo '</div>' . "\n";
@@ -546,25 +553,32 @@ function print_project_menu_bar() {
 }
 
 /**
- * Print the menu bar with a list of projects to which the user has access
- * @todo check parents param - set_project.php?project_id=' . $p_parents . $t_subproject
+ * Print the menu bar with a list of subprojects to which the user has access
+ *
  * @param integer $p_current_project_id Selected project id.
- * @param integer $p_parent_project_id Parent project id.
- * @param string  $p_parents    Parent project identifiers.
+ * @param integer $p_parent_project_id  Parent project id.
+ * @param array   $p_parents            Parent project identifiers.
+ *
  * @return void
  */
-function print_subproject_menu_bar( $p_current_project_id, $p_parent_project_id, $p_parents = '' ) {
+function print_subproject_menu_bar( $p_current_project_id, $p_parent_project_id, array $p_parents = array() ) {
 	$t_subprojects = current_user_get_accessible_subprojects( $p_parent_project_id );
 
 	foreach( $t_subprojects as $t_subproject_id ) {
-		$t_active = $p_current_project_id == $t_subproject_id ? 'active' : '';
-		echo '<a class="btn btn-xs btn-white btn-default ' . $t_active .
-			'" href="' . helper_mantis_url( 'set_project.php?project_id=' . $p_parents . $t_subproject_id ) .
-			'"><i class="ace-icon fa fa-angle-double-right"></i> ' .
-			string_html_specialchars( project_get_field( $t_subproject_id, 'name' ) ) . '</a>';
+		echo project_link_for_menu(
+				$t_subproject_id,
+				$t_subproject_id == $p_current_project_id,
+				'btn btn-xs btn-white btn-info',
+				$p_parents,
+				'<i class="ace-icon fa fa-angle-double-right"></i>'
+			);
 
-		# Render this subproject's subprojects ... passing current project id to highlight selected project
-		print_subproject_menu_bar( $p_current_project_id, $t_subproject_id, $p_parents . $t_subproject_id . ';' );
+		# Recursive call to render this subproject's subprojects
+		print_subproject_menu_bar(
+			$p_current_project_id,
+			$t_subproject_id,
+			array_merge( $p_parents, array( $t_subproject_id) )
+		);
 	}
 }
 

core/project_api.php
@@ -936,3 +936,39 @@ function project_format_id( $p_project_id ) {
 	$t_padding = config_get( 'display_project_padding' );
 	return( utf8_str_pad( $p_project_id, $t_padding, '0', STR_PAD_LEFT ) );
 }
+
+
+/**
+ * Returns the project name as a link formatted for display in menus and buttons.
+ *
+ * The link is formatted as a link to set_project.php, which can be used to
+ * display project selection menus:
+ * - projects list in navbar {@see layout_navbar_projects_menu()}
+ * - project menu bar {@see print_project_menu_bar()}
+ *
+ * @param integer $p_project_id Project Id to display
+ * @param bool    $p_active     True if it's the currently active project
+ * @param string  $p_class      CSS classes to apply
+ * @param array   $p_parents    Array of parent projects (empty if top-level)
+ * @param string  $p_indent     String to use to indent the subprojects
+ *
+ * @return string Fully formatted HTML link to the project
+ */
+function project_link_for_menu( $p_project_id, $p_active = false, $p_class = '', array $p_parents = array(), $p_indent = '' ) {
+	if( $p_parents ) {
+		$t_full_id = implode( ";", $p_parents ) . ';' . $p_project_id;
+		$t_indent = str_repeat( $p_indent, count( $p_parents ) ) . '&nbsp;';
+	} else {
+		$t_full_id = $p_project_id;
+		$t_indent = '';
+	}
+
+	$t_url = helper_mantis_url( 'set_project.php?project_id=' . $t_full_id );
+	$t_label = $t_indent . string_html_specialchars( project_get_name( $p_project_id ) );
+
+	if( $p_active ) {
+		$p_class .= ' active';
+	}
+
+	return sprintf('<a class="%s" href="%s">%s</a>', $p_class, $t_url, $t_label );
+}

commit 2d2d5ef4d4ae3224a78b27373cd0d2a5e5bbfc3f

Fix search in project selector
Commit 8b28db636a31e04f3db1eb4d3aae5d3489a46301 (issue #26888) introduced
a regression causing the filtered projects list to always be empty,
regardless of what the user typed in the Search field.

The bug was caused by a typo in CSS class name (`project_link` instead
of `project-link`).

Fixes #27375

core/layout_api.php
@@ -604,14 +604,14 @@ function layout_navbar_projects_list( $p_project_id = null, $p_include_all_proje
 
 	if( $p_include_all_projects && $p_filter_project_id !== ALL_PROJECTS ) {
 		echo '<li>';
-		echo project_link_for_menu( ALL_PROJECTS, false, 'project_link' );
+		echo project_link_for_menu( ALL_PROJECTS, false, 'project-link' );
 		echo "</li>\n";
 		echo '<li class="divider"></li>' . "\n";
 	}
 
 	foreach( $t_project_ids as $t_id ) {
 		echo 0 == strcmp( $t_id, $p_project_id ) ? '<li class="active">' : '<li>';
-		echo project_link_for_menu( $t_id, false, 'project_link' );
+		echo project_link_for_menu( $t_id, false, 'project-link' );
 		echo "</li>\n";
 		layout_navbar_subproject_option_list( $t_id, $p_project_id, $p_filter_project_id, $p_trace );
 	}
@@ -645,7 +645,7 @@ function layout_navbar_subproject_option_list( $p_parent_id, $p_project_id = nul
 		}
 
 		echo 0 == strcmp( $p_project_id, $t_full_id ) ? '<li class="active">' : '<li>';
-		echo project_link_for_menu( $t_id, false, 'project_link', $p_parents, $t_indent );
+		echo project_link_for_menu( $t_id, false, 'project-link', $p_parents, $t_indent );
 		echo "</li>\n";
 
 		layout_navbar_subproject_option_list( $t_id, $p_project_id, $p_filter_project_id, $p_trace, $p_parents );