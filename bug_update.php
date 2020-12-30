Inspirati/doctis

commit 49eb8aaf877ba5caa82e9010c03ff0e8c5f55076

Fix access checks for assign and change status
- Users should be able to assign issues even if they can’t update issues.
- Users should be able to change status even if they can’t update issues.

Fixes #21393, #22093

bug_update.php
@@ -152,16 +152,34 @@ $t_reporter_reopening =
 	access_can_reopen_bug( $t_existing_bug, $t_current_user_id );
 
 if ( !$t_reporter_reopening && !$t_reporter_closing ) {
-	# Ensure that the user has permission to update bugs. This check also factors
-	# in whether the user has permission to view private bugs. The
-	# $g_limit_reporters option is also taken into consideration.
-	access_ensure_bug_level( config_get( 'update_bug_threshold' ), $f_bug_id );
+	switch( $f_update_type ) {
+		case BUG_UPDATE_TYPE_ASSIGN:
+			access_ensure_bug_level( 'update_bug_assign_threshold', $f_bug_id );
+			$t_check_readonly = true;
+			break;
+		case BUG_UPDATE_TYPE_CLOSE:
+		case BUG_UPDATE_TYPE_REOPEN:
+			access_ensure_bug_level( 'update_bug_status_threshold', $f_bug_id );
+			$t_check_readonly = false;
+			break;
+		case BUG_UPDATE_TYPE_CHANGE_STATUS:
+			access_ensure_bug_level( 'update_bug_status_threshold', $f_bug_id );
+			$t_check_readonly = true;
+			break;
+		case BUG_UPDATE_TYPE_NORMAL:
+		default:
+			access_ensure_bug_level( 'update_bug_threshold', $f_bug_id );
+			$t_check_readonly = true;
+			break;
+	}
 
-	# Check if the bug is in a read-only state and whether the current user has
-	# permission to update read-only bugs.
-	if( bug_is_readonly( $f_bug_id ) ) {
-		error_parameters( $f_bug_id );
-		trigger_error( ERROR_BUG_READ_ONLY_ACTION_DENIED, ERROR );
+	if( $t_check_readonly ) {
+		# Check if the bug is in a read-only state and whether the current user has
+		# permission to update read-only bugs.
+		if( bug_is_readonly( $f_bug_id ) ) {
+			error_parameters( $f_bug_id );
+			trigger_error( ERROR_BUG_READ_ONLY_ACTION_DENIED, ERROR );
+		}
 	}
 }
 

commit cf37d1a9a525b33a2cf36ec394b7a8de7a845be1

Properly check access level when updating issues
Commit 49eb8aaf877ba5caa82e9010c03ff0e8c5f55076 (issues #21393, #22093)
introduced a bug in the access level check, as it passed the threshold
as a string to access_ensure_bug_level() instead of its actual value.

This seemed to work in PHP < 8.0, because string comparisons behaved
differently [[1]], but in reality there was no actual access check, as
the strings were evaluated to 0 (ANYBODY).

Adding the missing config_get() call to fix the access level check.

Fixes #27806

[1]: https://www.php.net/releases/8.0/en.php#consistent-type-errors-for-internal-functions

bug_update.php
@@ -154,24 +154,25 @@ $t_reporter_reopening =
 if ( !$t_reporter_reopening && !$t_reporter_closing ) {
 	switch( $f_update_type ) {
 		case BUG_UPDATE_TYPE_ASSIGN:
-			access_ensure_bug_level( 'update_bug_assign_threshold', $f_bug_id );
+			$t_threshold = 'update_bug_assign_threshold';
 			$t_check_readonly = true;
 			break;
 		case BUG_UPDATE_TYPE_CLOSE:
 		case BUG_UPDATE_TYPE_REOPEN:
-			access_ensure_bug_level( 'update_bug_status_threshold', $f_bug_id );
+			$t_threshold = 'update_bug_status_threshold';
 			$t_check_readonly = false;
 			break;
 		case BUG_UPDATE_TYPE_CHANGE_STATUS:
-			access_ensure_bug_level( 'update_bug_status_threshold', $f_bug_id );
+			$t_threshold = 'update_bug_status_threshold';
 			$t_check_readonly = true;
 			break;
 		case BUG_UPDATE_TYPE_NORMAL:
 		default:
-			access_ensure_bug_level( 'update_bug_threshold', $f_bug_id );
+			$t_threshold = 'update_bug_threshold';
 			$t_check_readonly = true;
 			break;
 	}
+	access_ensure_bug_level( config_get( $t_threshold ), $f_bug_id );
 
 	if( $t_check_readonly ) {
 		# Check if the bug is in a read-only state and whether the current user has