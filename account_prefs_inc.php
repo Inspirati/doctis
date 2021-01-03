Inspirati/doctis

commit 8af91a8ff870fead6fd84aa86d26f8b9dd7f48f6

Fix markup in account_prefs_inc.php
The sequence of wrapping divs was different from the usual layout,
causing inconsistent spacing when the account preferences section
was displayed on manage_user_edit_page.php.

edit_account_prefs() no longer prints the outer div, letting the parent
page take care of it.

Fixes #27574

account_prefs_inc.php
@@ -93,17 +93,8 @@ function edit_account_prefs( $p_user_id = null, $p_error_if_protected = true, $p
 	}
 ?>
 
-<div class="col-md-12 col-xs-12">
 <div class="space-10"></div>
-
-<div id="account-prefs-update-div" class="form-container">
-	<form id="account-prefs-update-form" method="post" action="account_prefs_update.php" class="form-inline">
-		<fieldset>
-			<?php echo form_security_field( 'account_prefs_update' ) ?>
-			<input type="hidden" name="user_id" value="<?php echo $p_user_id ?>" />
-			<input type="hidden" name="redirect_url" value="<?php echo $t_redirect_url ?>" />
-
-	<div class="widget-box widget-color-blue2">
+<div class="widget-box widget-color-blue2">
 	<div class="widget-header widget-header-small">
 		<h4 class="widget-title lighter">
 			<i class="ace-icon fa fa-sliders"></i>
@@ -112,8 +103,15 @@ function edit_account_prefs( $p_user_id = null, $p_error_if_protected = true, $p
 	</div>
 
 	<div class="widget-body">
-	<div class="widget-main no-padding">
-	<div class="table-responsive">
+		<div class="widget-main no-padding">
+			<div id="account-prefs-update-div" class="form-container">
+				<form id="account-prefs-update-form" method="post" action="account_prefs_update.php" class="form-inline">
+					<fieldset>
+						<?php echo form_security_field( 'account_prefs_update' ) ?>
+						<input type="hidden" name="user_id" value="<?php echo $p_user_id ?>" />
+						<input type="hidden" name="redirect_url" value="<?php echo $t_redirect_url ?>" />
+
+						<div class="table-responsive">
 	<table class="table table-bordered table-condensed table-striped">
 
 	<tr>
@@ -395,22 +393,28 @@ function edit_account_prefs( $p_user_id = null, $p_error_if_protected = true, $p
 	</tr>
 	<?php event_signal( 'EVENT_ACCOUNT_PREF_UPDATE_FORM', array( $p_user_id ) ); ?>
 	</table>
-	</div>
-	</div>
-	<div class="widget-toolbox padding-8 clearfix">
-		<input type="submit" class="btn btn-primary btn-white btn-round" value="<?php echo lang_get( 'update_prefs_button' ) ?>" />
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+		<div class="widget-toolbox padding-8 clearfix">
+			<button form="account-prefs-update-form"
+					class="btn btn-primary btn-white btn-round">
+				<?php echo lang_get( 'update_prefs_button' ) ?>
+			</button>
 
-		<?php echo form_security_field( 'account_prefs_reset' ) ?>
-		<input type="submit" class="btn btn-primary btn-white btn-round"
-			   formaction="account_prefs_reset.php"
-			   value="<?php echo lang_get( 'reset_prefs_button' ) ?>" />
+<?php
+	print_form_button(
+		'account_prefs_reset.php',
+		lang_get( 'reset_prefs_button' ),
+		array( 'user_id' => $p_user_id, 'redirect_url' => $t_redirect_url ),
+		null,
+		'btn btn-primary btn-white btn-round'
+	);
+?>
+		</div>
 	</div>
-	</div>
-	</div>
-		</fieldset>
-	</form>
-</div>
-
 </div>
 
 <?php

account_prefs_page.php
@@ -66,7 +66,14 @@ include( dirname( __FILE__ ) . '/account_prefs_inc.php' );
 layout_page_header( lang_get( 'change_preferences_link' ) );
 
 layout_page_begin();
+?>
 
-edit_account_prefs();
+<div class="col-md-12 col-xs-12">
+<div class="space-10"></div>
 
+<?php edit_account_prefs(); ?>
+
+</div>
+
+<?php
 layout_page_end();

manage_user_edit_page.php
@@ -367,10 +367,21 @@ if( $t_reset || $t_unlock || $t_delete || $t_impersonate ) {
 
 <?php
 } # End of PROJECT ACCESS conditional section
-echo '</div>';
+?>
 
+<!-- ACCOUNT PREFERENCES -->
+<?php
 define( 'ACCOUNT_PREFS_INC_ALLOW', true );
 include( dirname( __FILE__ ) . '/account_prefs_inc.php' );
-edit_account_prefs( $t_user['id'], false, false, 'manage_user_edit_page.php?user_id=' . $t_user_id );
+edit_account_prefs(
+	$t_user['id'],
+	false,
+	false,
+	'manage_user_edit_page.php?user_id=' . $t_user_id
+);
+?>
 
+</div>
+
+<?php
 layout_page_end();

commit 615cb7f732ba1430bff29f49a43f332afb5f209b

Fix markup in account_prefs_inc.php (take 2)
Commit 8af91a8ff870fead6fd84aa86d26f8b9dd7f48f6 introduced a regression
in the user preferences on account_prefs_page.php.

This fixes the problem, by printing the wrapping div and spacer within
the edit_account_prefs() function again, but only when the Accounts menu
is shown (p_accounts_menu == true).

Fixes #27574

account_prefs_inc.php
@@ -90,6 +90,7 @@ function edit_account_prefs( $p_user_id = null, $p_error_if_protected = true, $p
 <?php
 	if( $p_accounts_menu ) {
 		print_account_menu( 'account_prefs_page.php' );
+		echo '<div class="col-md-12 col-xs-12">';
 	}
 ?>
 
@@ -418,4 +419,7 @@ function edit_account_prefs( $p_user_id = null, $p_error_if_protected = true, $p
 </div>
 
 <?php
+	if( $p_accounts_menu ) {
+		echo '</div>';
+	}
 } # end of edit_account_prefs()

account_prefs_page.php
@@ -66,14 +66,5 @@ include( dirname( __FILE__ ) . '/account_prefs_inc.php' );
 layout_page_header( lang_get( 'change_preferences_link' ) );
 
 layout_page_begin();
-?>
-
-<div class="col-md-12 col-xs-12">
-<div class="space-10"></div>
-
-<?php edit_account_prefs(); ?>
-
-</div>
-
-<?php
+edit_account_prefs();
 layout_page_end();