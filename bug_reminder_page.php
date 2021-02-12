Inspirati/doctis

commit de956adde2293dccdfc173036ca3384ce4b0602e

Use new icon API functions
Replace hardcoded fontawesome <i> tags with new print_icon() or
icon_get() calls.

Fixes #27828

bug_reminder_page.php
@@ -78,7 +78,7 @@ layout_page_begin();
 <div class="widget-box widget-color-blue2">
 <div class="widget-header widget-header-small">
 	<h4 class="widget-title lighter">
-		<i class="ace-icon fa fa-envelope"></i>
+		<?php print_icon( 'fa-envelope', 'ace-icon' ); ?>
 		<?php echo lang_get( 'bug_reminder' ) ?>
 	</h4>
 </div>
@@ -158,8 +158,9 @@ layout_page_begin();
 	</form>
 	<br>
 	<div class="alert alert-info">
-		<p><i class="fa fa-info-circle fa-lg"> </i>
+		<p>
 		<?php
+			print_icon( 'fa-info-circle', 'fa-lg' ); ?>
 			echo lang_get( 'reminder_explain' ) . ' ';
 			if( ON == config_get( 'reminder_recipients_monitor_bug' ) ) {
 				echo lang_get( 'reminder_monitor' ) . ' ';

commit 32eab26d42241aac91ace7e5032e77549d5b2ea4

Stray `?>` tag causes PHP code to display
Regression introduced by de956adde2293dccdfc173036ca3384ce4b0602e.

Fixes #27828

bug_reminder_page.php
@@ -160,8 +160,8 @@ layout_page_begin();
 	<div class="alert alert-info">
 		<p>
 		<?php
-			print_icon( 'fa-info-circle', 'fa-lg' ); ?>
-			echo lang_get( 'reminder_explain' ) . ' ';
+			print_icon( 'fa-info-circle', 'fa-lg' );
+			echo ' ' . lang_get( 'reminder_explain' ) . ' ';
 			if( ON == config_get( 'reminder_recipients_monitor_bug' ) ) {
 				echo lang_get( 'reminder_monitor' ) . ' ';
 			}