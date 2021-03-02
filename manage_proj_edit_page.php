Inspirati/doctis

commit 83801f0e9e4e1324dab4d74bc7d3d95c64e66288

New generic 'edit' string, remove duplicates
There were several language strings defined for the same label:
edit_link, bugnote_edit_link, update_bug_button.

Adding a new generic short string and replace all occurences of the old
ones in the code.

manage_proj_edit_page.php
@@ -344,7 +344,7 @@ if ( config_get( 'subprojects_enabled') == ON ) {
 							<div class="inline">
 							<?php print_link_button(
 								'manage_proj_edit_page.php?project_id=' . $t_subproject['id'],
-								lang_get( 'edit_link' ), 'btn-xs' );
+								lang_get( 'edit' ), 'btn-xs' );
 							?>
 							<?php print_link_button(
 								"manage_proj_subproj_delete.php?project_id=$f_project_id&subproject_id=" . $t_subproject['id'] . form_security_param( 'manage_proj_subproj_delete' ),
@@ -432,7 +432,7 @@ if ( config_get( 'subprojects_enabled') == ON ) {
 						$t_id = urlencode( $t_id );
 						$t_project_id = urlencode( $f_project_id );
 						echo '<div class="pull-left">';
-						print_form_button( 'manage_proj_cat_edit_page.php?id=' . $t_id . '&project_id=' . $t_project_id, lang_get( 'edit_link' ),
+						print_form_button( 'manage_proj_cat_edit_page.php?id=' . $t_id . '&project_id=' . $t_project_id, lang_get( 'edit' ),
 							null, null, 'btn btn-xs btn-primary btn-white btn-round' );
 						echo '</div>';
 					} ?>
@@ -533,7 +533,7 @@ if ( config_get( 'subprojects_enabled') == ON ) {
 					$t_version_id = version_get_id( $t_name, $f_project_id );
 					if( !$t_inherited ) {
 						echo '<div class="pull-left">';
-						print_form_button( 'manage_proj_ver_edit_page.php?version_id=' . $t_version_id, lang_get( 'edit_link' ) );
+						print_form_button( 'manage_proj_ver_edit_page.php?version_id=' . $t_version_id, lang_get( 'edit' ) );
 						echo '</div>';
 
 						echo '<div class="pull-left">';

account_prof_menu_page.php
@@ -177,7 +177,7 @@ if( $g_global_profiles ) {
 					echo '<div class="pull-left">';
 					print_form_button(
 						'account_prof_edit_page.php',
-						lang_get( 'edit_link' ),
+						lang_get( 'edit' ),
 						$t_param
 					);
 					echo '</div>';

adm_config_report.php
@@ -420,7 +420,7 @@ while( $t_row = $t_config_query->fetch() ) {
 			$t_action_params['action'] = MANAGE_CONFIG_ACTION_EDIT;
 			$t_url_edit = helper_url_combine( 'adm_config_page.php', http_build_query( $t_action_params ) );
 			echo '<div class="pull-left">';
-			print_link_button( $t_url_edit, lang_get( 'edit_link' ), 'btn-xs' );
+			print_link_button( $t_url_edit, lang_get( 'edit' ), 'btn-xs' );
 			echo '</div>';
 
 			# Clone button

bug_update_page.php
@@ -294,7 +294,7 @@ if( $t_show_reporter || $t_show_handler || $t_show_due_date ) {
 				echo '</select>';
 			} else {
 				echo string_attribute( user_get_name( $t_bug->reporter_id ) );
-				echo ' [<a href="#reporter_edit" class="click-url" url="' . string_get_bug_update_url( $f_bug_id ) . '&amp;reporter_edit=true">' . lang_get( 'edit_link' ) . '</a>]';
+				echo ' [<a href="#reporter_edit" class="click-url" url="' . string_get_bug_update_url( $f_bug_id ) . '&amp;reporter_edit=true">' . lang_get( 'edit' ) . '</a>]';
 			}
 		}
 		echo '</td>';

bug_view_inc.php
@@ -1174,7 +1174,7 @@ function bug_view_action_buttons( $p_bug_id, $p_flags ) {
 	# UPDATE button
 	if( $p_flags['can_update'] ) {
 		echo '<div class="pull-left padding-right-8">';
-		html_button( string_get_bug_update_page(), lang_get( 'update_bug_button' ), array( 'bug_id' => $p_bug_id ) );
+		html_button( string_get_bug_update_page(), lang_get( 'edit' ), array( 'bug_id' => $p_bug_id ) );
 		echo '</div>';
 	}
 

bugnote_view_inc.php
@@ -205,7 +205,7 @@ $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 				echo '<div class="pull-left">';
 				print_form_button(
 					'bugnote_edit_page.php',
-					lang_get( 'bugnote_edit_link' ),
+					lang_get( 'edit' ),
 					array( 'bugnote_id' => $t_activity['id'] ),
 					OFF );
 				echo '</div>';

core/columns_api.php
@@ -1161,7 +1161,7 @@ function print_column_edit( BugData $p_bug, $p_columns_target = COLUMNS_TARGET_V
 	if( !bug_is_readonly( $p_bug->id ) && access_has_bug_level( config_get( 'update_bug_threshold' ), $p_bug->id ) ) {
 		echo '<a href="' . string_get_bug_update_url( $p_bug->id ) . '">';
 		echo '<i class="fa fa-pencil bigger-130 padding-2 grey"';
-		echo ' title="' . lang_get( 'update_bug_button' ) . '"></i></a>';
+		echo ' title="' . lang_get( 'edit' ) . '"></i></a>';
 	} else {
 		echo '&#160;';
 	}

manage_filter_page.php
@@ -115,7 +115,7 @@ function table_print_filter_row( $p_filter_id ) {
 	echo '</div>';
 	if( $t_editable ) {
 		echo '<div class="pull-left">';
-		print_form_button( 'manage_filter_edit_page.php', lang_get( 'edit_link' ), array( 'filter_id' =>  $p_filter_id ) );
+		print_form_button( 'manage_filter_edit_page.php', lang_get( 'edit' ), array( 'filter_id' =>  $p_filter_id ) );
 		echo '</div>';
 		echo '<div class="pull-left">';
 		print_form_button( 'manage_filter_delete.php', lang_get( 'delete_filter_button' ), array( 'filter_id' =>  $p_filter_id ) );

commit 7ecb252d804333eac8e47be25284b058808bc592

Fix missing language string 'edit_link'
Regression from 83801f0e9e4e1324dab4d74bc7d3d95c64e66288.

Fixes #27298

manage_proj_edit_page.php
@@ -813,7 +813,7 @@ event_signal( 'EVENT_MANAGE_PROJECT_PAGE', array( $f_project_id ) );
 				if( $t_can_manage_this_user ) {
 					echo '<div class="editable_access_level">';
 					echo $t_current_level_string . '<span class="hidden unchanged">';
-					echo ' <a href="#" class="edit_link">[' . lang_get( 'edit_link' ) . ']</a>';
+					echo ' <a href="#" class="edit_link">[' . lang_get( 'edit' ) . ']</a>';
 					echo '</span>';
 					$t_arrow = layout_is_rtl() ? 'fa-long-arrow-left' : 'fa-long-arrow-right';
 					echo ' <span class="changed_to">';