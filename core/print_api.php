Inspirati/doctis

commit b2e9be3b4d887ef394997a7c8c7884cbd01d3260

Display a message with the Proceed button
When delayed inline error messages are displayed on a page with a
redirection, call html_operation_successful() to inform the user that
the operation completed successfully despite the errors.

Issue #33480

core/print_api.php
@@ -102,6 +102,7 @@ function print_header_redirect( $p_url, $p_die = true, $p_sanitize = false, $p_a
 		# html_meta_redirect(), called by layout_page_header().
 		layout_page_header( null, $p_url );
 		layout_page_begin();
+		html_operation_successful( $p_url );
 		echo '<br /><div class="center">';
 		print_link_button( $p_url, lang_get( 'proceed' ) );
 		echo '</div>';

commit 46a37ae5e04d5d2ade47b21b19b5cd4faf882db2

Remove extra "Proceed" button when redirecting
When print_header_redirect() is handling pending errors, the Proceed
button is shown twice, once in html_operation_successful()'s output, and
again in a standalone div.

This is a regression introduced by b2e9be3b4d887ef394997a7c8c7884cbd01d3260
(issue #33480), which added the html_operation_successful() call, but
did not remove the existing Proceed button.

Fixes #34404

core/print_api.php
@@ -103,9 +103,6 @@ function print_header_redirect( $p_url, $p_die = true, $p_sanitize = false, $p_a
 		layout_page_header( null, $p_url );
 		layout_page_begin();
 		html_operation_successful( $p_url );
-		echo '<br /><div class="center">';
-		print_link_button( $p_url, lang_get( 'proceed' ) );
-		echo '</div>';
 		layout_page_end();
 		return false;
 	}