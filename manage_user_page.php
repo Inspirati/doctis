mantisbt/mantisbt

Display failed_login_count only if max_failed_login_count is enabled
Issue #26797

manage_user_page.php
@@ -342,9 +342,12 @@ $t_user_count = count( $t_users );
 	# Print column headers with sort links
 	$t_columns = array(
 		'username', 'realname', 'email', 'access_level',
-		'enabled', 'protected', 'date_created', 'last_visit', 'failed_login_count'
+		'enabled', 'protected', 'date_created', 'last_visit'
 	);
-
+	$t_display_failed_login_count = OFF != config_get( 'max_failed_login_count' );
+	if( $t_display_failed_login_count ) {
+		$t_columns[] = 'failed_login_count';
+	}
 	foreach( $t_columns as $t_col ) {
 		echo "\t<th>";
 		print_manage_user_sort_link( 'manage_user_page.php',
@@ -393,8 +396,8 @@ $t_user_count = count( $t_users );
 					} ?>
 				</td>
 				<td><?php echo $u_date_created ?></td>
-				<td><?php echo $u_last_visit ?></td>
-				<td><?php echo $u_failed_login_count ?></td>
+				<td><?php echo $u_last_visit ?></td><?php if( $t_display_failed_login_count ) { ?>
+				<td><?php echo $u_failed_login_count ?></td><?php } ?>
 			</tr>
 <?php
 	}  # end for

Inspirati/doctis

manage_user_page.php
@@ -357,9 +357,12 @@ $t_user_count = count( $t_users );
 	# Print column headers with sort links
 	$t_columns = array(
 		'username', 'realname', 'email', 'access_level',
-		'enabled', 'protected', 'date_created', 'last_visit', 'failed_login_count'
+		'enabled', 'protected', 'date_created', 'last_visit'
 	);
-
+	$t_display_failed_login_count = OFF != config_get( 'max_failed_login_count' );
+	if( $t_display_failed_login_count ) {
+		$t_columns[] = 'failed_login_count';
+	}
 	foreach( $t_columns as $t_col ) {
 		echo "\t<th>";
 		print_manage_user_sort_link( 'manage_user_page.php',
@@ -435,8 +438,8 @@ $t_user_count = count( $t_users );
 					} ?>
 				</td>
 				<td><?php echo $v_date_created ?></td>
-				<td><?php echo $v_last_visit ?></td>
-				<td><?php echo $v_failed_login_count ?></td>
+				<td><?php echo $v_last_visit ?></td><?php if( $t_display_failed_login_count ) { ?>
+				<td><?php echo $v_failed_login_count ?></td><?php } ?>
 			</tr>
 <?php
 	}  # end for