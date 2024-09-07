Inspirati/doctis

commit ff0063a21fec840bf29188cc7d7acf06427f76a2

New $g_graphviz_path config
Replaces $g_dot_tool and $g_neato_tool which are now obsolete.

Define new constants in Graph class to specify the individual tool to
use when instantiating the class; the graphviz tool path is built
by the constructor.

Adapted usage in relationship graph API and workflow graph.

Updated documentation

Fixes #34609

core/graphviz_api.php
@@ -47,6 +47,15 @@ require_api( 'utility_api.php' );
 class Graph {
 
 	/**
+	 * Constants defining the Graphviz tools' names.
+	 *
+	 * These are the names of the executables in the directory defined by
+	 * {@see $g_graphviz_path}.
+	 */
+	const TOOL_DOT = 'dot';
+	const TOOL_NEATO = 'neato';
+
+	/**
 	 * Constant(s) defining the output formats supported by dot and neato.
 	 */
 	const GRAPHVIZ_ATTRIBUTED_DOT = 0;
@@ -215,14 +224,15 @@ class Graph {
 	 * @param array  $p_attributes Attributes
 	 * @param string $p_tool       Graph generation tool (one of the TOOL_* constants)
 	 */
-	public function __construct( $p_name = 'G', array $p_attributes = array(), $p_tool = 'neato' ) {
+	public function __construct( $p_name = 'G', array $p_attributes = array(), $p_tool = Graph::TOOL_NEATO ) {
 		if( is_string( $p_name ) ) {
 			$this->name = $p_name;
 		}
 
 		$this->set_attributes( $p_attributes );
 
-		$this->graphviz_tool = $p_tool;
+		$t_graphviz_path = config_get_global( 'graphviz_path' );
+		$this->graphviz_tool = $t_graphviz_path . $p_tool;
 	}
 
 	/**

config_defaults_inc.php
@@ -4470,43 +4470,34 @@ $g_rss_enabled = ON;
 /**
  * Enable relationship graphs support.
  *
- * Show issue relationships using graphs.
+ * Show issue relationships and workflow transitions using graphs.
  *
  * In order to use this feature, you must first install GraphViz.
  * @see https://www.graphviz.org/ Graphviz homepage
- *
- * Refer to the notes near the top of core/graphviz_api.php and
- * core/relationship_graph_api.php for more information.
+ * @see $g_graphviz_path
  *
  * @global int $g_relationship_graph_enable
  */
 $g_relationship_graph_enable = OFF;
 
 /**
- * Complete path to dot and neato tools.
+ * Complete path to the Graphviz tools.
  *
- * Your webserver must have execute permission to these programs in order to
- * generate relationship graphs.
+ * {@see https://graphviz.org/ Graphviz} must be installed on your server.
+ * Mantis uses the following tools :
+ * - Relationship graphs: dot, neato
+ * - Workflow transitions graph: dot
  *
- * NOTE: On windows, the IIS user may require permissions to cmd.exe to be able
- * to use PHP's proc_open.
+ * NOTES:
+ * - Requires trailing `/`
+ * - The webserver must have execute permission to these programs in order to
+ *   generate the graphs.
+ * - On Windows, the IIS user may require permissions to cmd.exe to be able
+ *   to use PHP's {@see proc_open()}
  *
- * @global string $g_dot_tool
+ * @global string $g_graphviz_path
  */
-$g_dot_tool = '/usr/bin/dot';
-
-/**
- * Complete path to dot and neato tools.
- *
- * Your webserver must have execute permission to these programs in order to
- * generate relationship graphs.
- *
- * NOTE: On windows, the IIS user may require permissions to cmd.exe to be able
- * to use PHP's proc_open.
- *
- * @global string $g_neato_tool
- */
-$g_neato_tool = '/usr/bin/neato';
+$g_graphviz_path = '/usr/bin/';
 
 /**
  * Font name and size, as required by Graphviz.
@@ -5173,7 +5164,6 @@ $g_global_settings = array(
 	'default_home_page',
 	'default_language',
 	'display_errors',
-	'dot_tool',
 	'email_dkim_domain',
 	'email_dkim_enable',
 	'email_dkim_identity',
@@ -5195,6 +5185,7 @@ $g_global_settings = array(
 	'fileinfo_magic_db_file',
 	'form_security_validation',
 	'global_settings',
+	'graphviz_path',
 	'hostname',
 	'html_valid_tags',
 	'html_valid_tags_single_line',
@@ -5226,7 +5217,6 @@ $g_global_settings = array(
 	'long_process_timeout',
 	'manage_config_cookie',
 	'manual_url',
-	'neato_tool',
 	'path',
 	'plugin_path',
 	'plugins_enabled',

core/obsolete.php
@@ -235,3 +235,5 @@ config_obsolete( 'display_project_padding' );
 
 # changes in 2.27.0
 config_obsolete( 'allow_file_cache' );
+config_obsolete( 'dot_tool', 'graphviz_path' );
+config_obsolete( 'neato_tool', 'graphviz_path' );

core/relationship_graph_api.php
@@ -156,7 +156,6 @@ function relgraph_generate_rel_graph( $p_bug_id, $p_show_summary = false ) {
 	$t_graph_fontsize = config_get( 'relationship_graph_fontsize' );
 	$t_graph_fontpath = get_font_path();
 	$t_view_on_click = config_get( 'relationship_graph_view_on_click' );
-	$t_neato_tool = config_get_global( 'neato_tool' );
 
 	$t_graph_attributes = array(
 		'overlap'	=> 'false',
@@ -167,7 +166,7 @@ function relgraph_generate_rel_graph( $p_bug_id, $p_show_summary = false ) {
 		$t_graph_attributes['fontpath'] = $t_graph_fontpath;
 	}
 
-	$t_graph = new Graph( $t_id_string, $t_graph_attributes, $t_neato_tool );
+	$t_graph = new Graph( $t_id_string, $t_graph_attributes );
 
 	$t_graph->set_default_node_attr( array (
 			'fontname'	=> $t_graph_fontname,
@@ -287,7 +286,6 @@ function relgraph_generate_dep_graph( $p_bug_id, $p_horizontal = false, $p_show_
 	$t_graph_fontsize = config_get( 'relationship_graph_fontsize' );
 	$t_graph_fontpath = get_font_path();
 	$t_view_on_click = config_get( 'relationship_graph_view_on_click' );
-	$t_dot_tool = config_get_global( 'dot_tool' );
 
 	$t_graph_attributes = array();
 
@@ -302,7 +300,7 @@ function relgraph_generate_dep_graph( $p_bug_id, $p_horizontal = false, $p_show_
 		$t_graph_orientation = 'vertical';
 	}
 
-	$t_graph = new Digraph( $t_id_string, $t_graph_attributes, $t_dot_tool );
+	$t_graph = new Digraph( $t_id_string, $t_graph_attributes, Graph::TOOL_DOT );
 
 	$t_graph->set_default_node_attr( array (
 			'fontname'	=> $t_graph_fontname,

docbook/Admin_Guide/en-US/config/relationship.xml
@@ -10,11 +10,12 @@
 		<emphasis>dependencies</emphasis> and a full
 		<emphasis>relationships</emphasis> graph.
 	</para>
+	<para>It is also possible to visualize the Workflow transitions.
+	</para>
 
 	<important>
 		<para>This feature relies on the external
-			<emphasis>dot</emphasis> and <emphasis>neato</emphasis> tools
-			from the <ulink url="https://www.graphviz.org/">GraphViz</ulink>
+			<ulink url="https://www.graphviz.org/">GraphViz</ulink>
 			library, which must be installed separately.
 		</para>
 		<para>Most Linux distributions have a GraphViz package available
@@ -39,6 +40,21 @@
 			</itemizedlist>
 		</para>
 	</important>
+
+	<para>The following Graphviz tools are used:</para>
+	<itemizedlist>
+		<listitem><para>
+			Relationship graphs: <literal>dot</literal>, <literal>neato</literal>
+		</para></listitem>
+		<listitem><para>
+			Workflow transitions graph (see
+			<xref linkend="admin.lifecycle.workflow.transitions" />):
+			<literal>dot</literal>
+		</para></listitem>
+	</itemizedlist>
+	<para>The webserver must have execute permission to these programs
+		in order to generate the graphs.
+	</para>
 
 	<variablelist>
 		<varlistentry>
@@ -100,25 +116,29 @@
 			</listitem>
 		</varlistentry>
 		<varlistentry>
-			<term>$g_dot_tool</term>
+			<term>$g_graphviz_path</term>
 			<listitem>
-				<para>
-					The full path for the dot tool.  The webserver must have execute
-					permission to this program in order to generate relationship graphs.
-					This configuration option is not relevant for Windows.  The default
-					value is '/usr/bin/dot'.
+				<para>Complete path to the
+					<ulink url="https://graphviz.org/ Graphviz">Graphviz</ulink> tools
+					(for details, see the note at the beginning of
+					<xref linkend="admin.config.relationship" />).
 				</para>
-			</listitem>
-		</varlistentry>
-		<varlistentry>
-			<term>$g_neato_tool</term>
-			<listitem>
-				<para>
-					The full path for the neato tool.  The webserver must have execute
-					permission to this program in order to generate relationship graphs.
-					This configuration option is not relevant for Windows.  The default
-					value is '/usr/bin/neato'.
+				<para>Requires trailing '/'.
+					The default	value is <literal>/usr/bin/</literal>.
 				</para>
+				<warning>
+					<itemizedlist>
+						<listitem><para>
+							The webserver must have execute permission to these programs
+							in order to generate the graphs.
+						</para></listitem>
+						<listitem><para>
+							On Windows, the IIS user may require permissions to
+							cmd.exe to be able to use PHP's
+							<ulink url="https://www.php.net/manual/en/function.proc-open.php">proc_open()</ulink>
+						</para></listitem>
+					</itemizedlist>
+				</warning>
 			</listitem>
 		</varlistentry>
 		<varlistentry>

workflow_graph_img.php
@@ -50,7 +50,6 @@ $t_status_arr  = MantisEnum::getAssocArrayIndexedByValues( config_get( 'status_e
 $t_graph_fontname = config_get( 'relationship_graph_fontname' );
 $t_graph_fontsize = config_get( 'relationship_graph_fontsize' );
 $t_graph_fontpath = get_font_path();
-$t_dot_tool = config_get_global( 'dot_tool' );
 
 $t_graph_attributes = array();
 
@@ -58,7 +57,7 @@ if( !empty( $t_graph_fontpath ) ) {
 	$t_graph_attributes['fontpath'] = $t_graph_fontpath;
 }
 
-$t_graph = new Graph( 'workflow', $t_graph_attributes, $t_dot_tool );
+$t_graph = new Graph( 'workflow', $t_graph_attributes, Graph::TOOL_DOT );
 
 $t_graph->set_default_node_attr( array ( 'fontname' => $t_graph_fontname,
 										 'fontsize' => $t_graph_fontsize,

commit 40ca13888134d5cd62d254af7b1554f83118b899

On Windows, GraphViz tools need '.exe' extension
Regression introduced by ff0063a21fec840bf29188cc7d7acf06427f76a2.

Fixes #34609

core/graphviz_api.php
@@ -51,6 +51,7 @@ class Graph {
 	 *
 	 * These are the names of the executables in the directory defined by
 	 * {@see $g_graphviz_path}.
+	 * On Windows, '.exe' extension will be appended.
 	 */
 	const TOOL_DOT = 'dot';
 	const TOOL_NEATO = 'neato';
@@ -239,6 +240,10 @@ class Graph {
 
 		$this->graphviz_path = config_get_global( 'graphviz_path' );
 		$this->graphviz_tool = $p_tool;
+
+		if( is_windows_server() ) {
+			$this->graphviz_tool .= '.exe';
+		}
 	}
 
 	/**