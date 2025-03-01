Inspirati/doctis

commit e2acdd5cfd70b906a10942268f7f64b8b776ab7e

Add plugin filters management in search.php
Before modification, filters created through plugins (using MantisFilter
class and EVENT_FILTER_FIELDS hook) cannot be used in "Permalink"
functionality.

Now, the data about filters mapped by EVENT_FILTER_FIELDS hook are
managed by search.php witch is used to analyse permanent links.

Fixes #25072

search.php
@@ -142,6 +142,28 @@ foreach( $_GET as $t_var_name => $t_var_value ) {
 
 $t_my_filter['custom_fields'] = $t_custom_fields;
 
+# Handle class-based filters defined as plugins
+$t_plugin_filters = filter_get_plugin_filters();
+foreach( $t_plugin_filters as $t_field_name => $t_filter_object ) {
+    switch( $t_filter_object->type ) {
+        case FILTER_TYPE_STRING:
+            $t_my_filter[$t_field_name] = gpc_get_string( $t_field_name, $t_meta_filter_any_array[$t_field_name] );
+            break;
+        case FILTER_TYPE_INT:
+            $t_my_filter[$t_field_name] = gpc_get_int( $t_field_name, $t_meta_filter_any_array[$t_field_name] );
+            break;
+        case FILTER_TYPE_BOOLEAN:
+            $t_my_filter[$t_field_name] = gpc_get_bool( $t_field_name, $t_meta_filter_any_array[$t_field_name]);
+            break;
+        case FILTER_TYPE_MULTI_STRING:
+            $t_my_filter[$t_field_name] = gpc_get_string_array( $t_field_name, $t_meta_filter_any_array[$t_field_name] );
+            break;
+        case FILTER_TYPE_MULTI_INT:
+            $t_my_filter[$t_field_name] = gpc_get_int_array( $t_field_name, $t_meta_filter_any_array[$t_field_name] );
+            break;
+    }
+}
+
 # Must use advanced filter so that the project_id is applied and multiple
 # selections are handled.
 $t_my_filter['_view_type'] = FILTER_VIEW_TYPE_ADVANCED;

commit da3a8ebd5f203fa4df82f6fede2e72888771caaa

Fix Undefined array key with Plugin Filters
When following a permalink including a Plugin Filter field, search.php
throws an "Undefined array key" SYSTEM WARNING.

This is due to an incorrect default in gpc_get_*() calls to get the
filter value, introduced in commit e2acdd5cfd70b906a10942268f7f64b8b776ab7e
(Issue #25072).

Fixes #35179

search.php
@@ -144,24 +144,24 @@ foreach( $_GET as $t_var_name => $t_var_value ) {
 
 $t_my_filter['custom_fields'] = $t_custom_fields;
 
-# Handle class-based filters defined as plugins
+# Handle class-based filters defined by plugins
 $t_plugin_filters = filter_get_plugin_filters();
 foreach( $t_plugin_filters as $t_field_name => $t_filter_object ) {
     switch( $t_filter_object->type ) {
         case FILTER_TYPE_STRING:
-            $t_my_filter[$t_field_name] = gpc_get_string( $t_field_name, $t_meta_filter_any_array[$t_field_name] );
+            $t_my_filter[$t_field_name] = gpc_get_string( $t_field_name, '' );
             break;
         case FILTER_TYPE_INT:
-            $t_my_filter[$t_field_name] = gpc_get_int( $t_field_name, $t_meta_filter_any_array[$t_field_name] );
+            $t_my_filter[$t_field_name] = gpc_get_int( $t_field_name, 0 );
             break;
         case FILTER_TYPE_BOOLEAN:
-            $t_my_filter[$t_field_name] = gpc_get_bool( $t_field_name, $t_meta_filter_any_array[$t_field_name]);
+            $t_my_filter[$t_field_name] = gpc_get_bool( $t_field_name );
             break;
         case FILTER_TYPE_MULTI_STRING:
-            $t_my_filter[$t_field_name] = gpc_get_string_array( $t_field_name, $t_meta_filter_any_array[$t_field_name] );
+            $t_my_filter[$t_field_name] = gpc_get_string_array( $t_field_name );
             break;
         case FILTER_TYPE_MULTI_INT:
-            $t_my_filter[$t_field_name] = gpc_get_int_array( $t_field_name, $t_meta_filter_any_array[$t_field_name] );
+            $t_my_filter[$t_field_name] = gpc_get_int_array( $t_field_name );
             break;
     }
 }