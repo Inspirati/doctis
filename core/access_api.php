Inspirati/doctis

commit 2b4990188ab3f55be420da5995a2b2c9cb385126

Fix Login URL from access denied page login button
When access_denied() is called from an admin page, the Login button
returns a 404 because auth_login_page() returns a relative URL and the
login page does not exist under /admin.

Fixes #35587

core/access_api.php
@@ -83,14 +83,14 @@ function access_denied() {
 	}
 
 	if( !auth_is_user_authenticated() ) {
-		print_header_redirect( auth_login_page( $t_return ) );
+		print_header_redirect( helper_mantis_url( auth_login_page( $t_return ) ) );
 	} else {
 		$t_buttons = [];
 		
 		# Login button
 		if( current_user_is_anonymous() ) {
 			$t_buttons[] = [
-				auth_login_page( $t_return ),
+				helper_mantis_url( auth_login_page( $t_return ) ),
 				lang_get( 'login' )
 			];
 		}

commit 73a663b6b516c36f43fbf3b53d87c0910b751106

Fix login page redirection from admin page
When an unauthenticated user tries to access the admin page, they are
redirected to the login page, but this was broken by use of absolute URL
introduced in commit 2b4990188ab3f55be420da5995a2b2c9cb385126.

Fixes #35587

core/access_api.php
@@ -83,7 +83,7 @@ function access_denied() {
 	}
 
 	if( !auth_is_user_authenticated() ) {
-		print_header_redirect( helper_mantis_url( auth_login_page( $t_return ) ) );
+		print_header_redirect( helper_mantis_url( auth_login_page( $t_return ) ), false, false, true );
 	} else {
 		$t_buttons = [];
 		